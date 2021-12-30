673e2f3b145102fbf05f925c96d8930e85484956
vanganur madhusudhan <89246442+madhu0951@users.noreply.github.com>
2021-12-30 17:49:35 +0530

    added code for saving output file in directory

--- face detection/faceDetection.py
+++ face detection/faceDetection.py
@@ -7,4 +7,5 @@ faces=face_cascade.detectMultiScale(gray,1.1,4)
 for (x,y,w,h) in faces:
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     cv2.imshow('img',img)
+    cv2.imwrite('face_detection_output.jpg', img)
     cv2.waitKey()
